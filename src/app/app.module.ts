1fba9662845c021d7161eb84397fa13224c69451
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-03-28 15:32:26 -0700

    Course viewer component is ready for course.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { CourseListComponent } from './course-list/course-list.component';
 import {FormsModule} from '@angular/forms';
 import {CourseServiceClient} from './services/CourseServiceClient';
+import { CourseViewerComponent } from './course-viewer/course-viewer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent
+    CourseListComponent,
+    CourseViewerComponent
   ],
   imports: [
     FormsModule,
