715c5bdc05d2e8bccfc124a00ffc01e8d1915b24
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-03-29 01:13:01 -0700

    Minor update.

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/xiaohai-client-angular'));
+app.use(express.static(__dirname + '/dist/wbdv-sp20-xiaohai-client-angular'));
 
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname + '/dist/xiaohai-client-angular/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/wbdv-sp20-xiaohai-client-angular/index.html'));
 })
 
 console.log('Console listening!');
