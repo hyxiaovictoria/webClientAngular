bff5c1d2f70bcc08c21951c6d5ed8b28419412ef
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-04-04 09:22:09 -0700

    Add selectQuizById().

--- src/app/services/QuizServiceClient.ts
+++ src/app/services/QuizServiceClient.ts
@@ -5,4 +5,8 @@ export class QuizServiceClient {
   findAllQuizzes = () =>
     fetch('http://localhost:3000/api/quizzes')
       .then(response => response.json());
+
+  findQuizById = (quizId) =>
+    fetch(`http://localhost:3000/api/quizzes/${quizId}`)
+      .then(response => response.json());
 }
