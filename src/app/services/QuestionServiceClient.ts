9f9eb34ec8e23cbc684519a71ba4ef38d1beeebc
Haiyan Xiao <xiao.hai@husky.neu.edu>
2020-04-05 14:27:37 -0700

    Minor changes.

--- src/app/services/QuestionServiceClient.ts
+++ src/app/services/QuestionServiceClient.ts
@@ -5,5 +5,4 @@ export class QuestionServiceClient {
   findQuestionsForQuiz = (quizId) =>
     fetch(`http://localhost:3000/api/quizzes/${quizId}/questions`)
     .then(response => response.json());
-
 }
